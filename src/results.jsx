d01cfdff2dc72806eac58789b54893452c51be79
tmarsh1245 <tmarsh@umass.edu>
2019-03-22 16:48:02 -0400

    state reactive table basics

---
+++ src/results.jsx
@@ -0,0 +1,117 @@
+// This is a place holder for the initial application state.
+const state = [
+
+];
+
+// This grabs the DOM element to be used to mount React components.
+var contentNode = document.getElementById("contents");
+// let blueWall = {name: "Blue Wall", price:"$$", distance:"On Campus", numberOfPeople:"Any", activityLvl:"low"};
+// let blueWallJson = JSON.stringify(blueWall);
+// let recCenter = {name: "Rec Center", price: "$", distance: "On Campus", numberOfPeople: "Any", activityLvl: "high"};
+// let recCenterJson = JSON.stringify(recCenter);
+
+class MyComponent extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data : [{name: "Blue Wall", price:"$$", distance:"On Campus", numberOfPeople:"Any", activityLvl:"low"}, 
+      {name: "Rec Center", price: "$", distance: "On Campus", numberOfPeople: "Any", activityLvl: "high"}]
+    }
+  }
+
+  render() {
+    let rows = this.state.data.map(location => {
+      return <LocationRow key = {
+        location.name
+      }
+      data = {
+        location
+      }
+      />
+    })
+    return (
+      <div>
+        <h1>Results</h1>
+        <hr />
+        <FilterList /> 
+        <ResultsTable />
+      </div>
+    );
+  }
+}
+
+class FilterList extends React.Component {
+  render(){
+    return (
+      <div>
+        List of filters
+      </div>
+    )
+  }
+}
+
+class ResultsTable extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data : [{name: "Blue Wall", price:"$$", distance:"On Campus", numberOfPeople:"Any", activityLvl:"low"}, 
+      {name: "Rec Center", price: "$", distance: "On Campus", numberOfPeople: "Any", activityLvl: "high"}]
+    }
+  }
+
+  render() {
+    let rows = this.state.data.map(location => {
+      return <LocationRow key = {
+        location.name
+      }
+      data = {
+        location
+      }
+      />
+    })
+    const borderedStyle = {border: "1px Solid Silver", padding: 6};
+    return (
+      <table>
+        <thead>
+          <tr style = {borderedStyle}>
+            <th>Location</th>
+            <th>Price</th>
+            <th>Distance</th>
+            <th>Number of People</th>
+            <th>Activity Level</th>
+          </tr>
+        </thead>
+        <tbody>
+          {rows}
+        </tbody>     
+        </table>
+    )
+  }
+}
+
+const LocationRow = (props) => {
+  return (
+    <tr>
+      <td>
+        { props.data.name}
+      </td>
+      <td>
+        {props.data.price}
+      </td>
+      <td>
+        {props.data.distance}
+      </td>
+      <td>
+        {props.data.numberOfPeople}
+      </td>
+      <td>
+        {props.data.activityLvl}
+      </td>
+    </tr>
+  )
+}
+
+
+
+// This renders the JSX component inside the content node:
+ReactDOM.render(<MyComponent />, contentNode);
---
+++ static/results.js
@@ -0,0 +1,188 @@
+"use strict";
+
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+// This is a place holder for the initial application state.
+var state = [];
+
+// This grabs the DOM element to be used to mount React components.
+var contentNode = document.getElementById("contents");
+// let blueWall = {name: "Blue Wall", price:"$$", distance:"On Campus", numberOfPeople:"Any", activityLvl:"low"};
+// let blueWallJson = JSON.stringify(blueWall);
+// let recCenter = {name: "Rec Center", price: "$", distance: "On Campus", numberOfPeople: "Any", activityLvl: "high"};
+// let recCenterJson = JSON.stringify(recCenter);
+
+var MyComponent = function (_React$Component) {
+  _inherits(MyComponent, _React$Component);
+
+  function MyComponent() {
+    _classCallCheck(this, MyComponent);
+
+    var _this = _possibleConstructorReturn(this, (MyComponent.__proto__ || Object.getPrototypeOf(MyComponent)).call(this));
+
+    _this.state = {
+      data: [{ name: "Blue Wall", price: "$$", distance: "On Campus", numberOfPeople: "Any", activityLvl: "low" }, { name: "Rec Center", price: "$", distance: "On Campus", numberOfPeople: "Any", activityLvl: "high" }]
+    };
+    return _this;
+  }
+
+  _createClass(MyComponent, [{
+    key: "render",
+    value: function render() {
+      var rows = this.state.data.map(function (location) {
+        return React.createElement(LocationRow, { key: location.name,
+          data: location
+        });
+      });
+      return React.createElement(
+        "div",
+        null,
+        React.createElement(
+          "h1",
+          null,
+          "Results"
+        ),
+        React.createElement("hr", null),
+        React.createElement(FilterList, null),
+        React.createElement(ResultsTable, null)
+      );
+    }
+  }]);
+
+  return MyComponent;
+}(React.Component);
+
+var FilterList = function (_React$Component2) {
+  _inherits(FilterList, _React$Component2);
+
+  function FilterList() {
+    _classCallCheck(this, FilterList);
+
+    return _possibleConstructorReturn(this, (FilterList.__proto__ || Object.getPrototypeOf(FilterList)).apply(this, arguments));
+  }
+
+  _createClass(FilterList, [{
+    key: "render",
+    value: function render() {
+      return React.createElement(
+        "div",
+        null,
+        "List of filters"
+      );
+    }
+  }]);
+
+  return FilterList;
+}(React.Component);
+
+var ResultsTable = function (_React$Component3) {
+  _inherits(ResultsTable, _React$Component3);
+
+  function ResultsTable() {
+    _classCallCheck(this, ResultsTable);
+
+    var _this3 = _possibleConstructorReturn(this, (ResultsTable.__proto__ || Object.getPrototypeOf(ResultsTable)).call(this));
+
+    _this3.state = {
+      data: [{ name: "Blue Wall", price: "$$", distance: "On Campus", numberOfPeople: "Any", activityLvl: "low" }, { name: "Rec Center", price: "$", distance: "On Campus", numberOfPeople: "Any", activityLvl: "high" }]
+    };
+    return _this3;
+  }
+
+  _createClass(ResultsTable, [{
+    key: "render",
+    value: function render() {
+      var rows = this.state.data.map(function (location) {
+        return React.createElement(LocationRow, { key: location.name,
+          data: location
+        });
+      });
+      var borderedStyle = { border: "1px Solid Silver", padding: 6 };
+      return React.createElement(
+        "table",
+        null,
+        React.createElement(
+          "thead",
+          null,
+          React.createElement(
+            "tr",
+            { style: borderedStyle },
+            React.createElement(
+              "th",
+              null,
+              "Location"
+            ),
+            React.createElement(
+              "th",
+              null,
+              "Price"
+            ),
+            React.createElement(
+              "th",
+              null,
+              "Distance"
+            ),
+            React.createElement(
+              "th",
+              null,
+              "Number of People"
+            ),
+            React.createElement(
+              "th",
+              null,
+              "Activity Level"
+            )
+          )
+        ),
+        React.createElement(
+          "tbody",
+          null,
+          rows
+        )
+      );
+    }
+  }]);
+
+  return ResultsTable;
+}(React.Component);
+
+var LocationRow = function LocationRow(props) {
+  return React.createElement(
+    "tr",
+    null,
+    React.createElement(
+      "td",
+      null,
+      props.data.name
+    ),
+    React.createElement(
+      "td",
+      null,
+      props.data.price
+    ),
+    React.createElement(
+      "td",
+      null,
+      props.data.distance
+    ),
+    React.createElement(
+      "td",
+      null,
+      props.data.numberOfPeople
+    ),
+    React.createElement(
+      "td",
+      null,
+      props.data.activityLvl
+    )
+  );
+};
+
+// This renders the JSX component inside the content node:
+ReactDOM.render(React.createElement(MyComponent, null), contentNode);
